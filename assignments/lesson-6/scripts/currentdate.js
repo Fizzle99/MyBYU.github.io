29e3d3052832657cb50a8a8e18fc745cdfba931c
Fizzle99 <CharlieBJS@gmail.com>
2019-02-17 23:05:53 -0600

    commit

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,14 +1,24 @@
-function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
 
 function compileDate() {
-
-var date = "2018";
-document.getElementById("demo").innerHTML = "&copy; 2020 | Attributions | Bradley James Sallee," + " " + date;
+var day = "Monday";
+var date = "18";
+var month = "February";
+var year = "2019";
+document.getElementById("demo").innerHTML = "&copy; 2020 | Attributions | Bradley James Sallee |" + " " + day + ", " + date + " " + month + " " + year;
 
 }
 
+function dynamicDate() {
+
+    var d = new Date();
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    
+    document.getElementById("demo2").innerHTML = "&copy; 2020 | Attributions | Bradley James Sallee | " + days[d.getDay()] + ", " + + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+    
+ }
+
 function experiment() {
-    document.getElementById("demo2").innerHTML = "Good stuff!";
+    document.getElementById("demo3").innerHTML = "Good stuff!";
 }
