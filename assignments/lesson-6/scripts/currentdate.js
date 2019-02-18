311754385897aeab9c9909e81c1559d83f8b3022
Fizzle99 <CharlieBJS@gmail.com>
2019-02-17 19:52:39 -0600

    commit

---
+++ assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+function compileDate() {
+
+var date = "2018";
+document.getElementById("demo").innerHTML = "&copy; 2020 | Attributions | Bradley James Sallee," + " " + date;
+
+}
+
+function experiment() {
+    document.getElementById("demo2").innerHTML = "Good stuff!";
+}
