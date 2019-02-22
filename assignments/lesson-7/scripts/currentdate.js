928360f1c7593ea562ccee692e5a7d5829058a1a
Fizzle99 <CharlieBJS@gmail.com>
2019-02-21 23:12:12 -0600

    commit

--- assignments/lesson-7/scripts/currentdate.js
+++ assignments/lesson-7/scripts/currentdate.js
@@ -1,14 +1,4 @@
 
-function compileDate() {
-var day = "Monday";
-var date = "18";
-var month = "February";
-var year = "2019";
-document.getElementById("demo").innerHTML = "&copy; 2020 | Attributions | Bradley James Sallee |" + " " + day + ", " + date + " " + month + " " + year;
-
-}
-
-function dynamicDate() {
 
     var d = new Date();
     var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
@@ -17,8 +7,4 @@ function dynamicDate() {
     
     document.getElementById("demo2").innerHTML = "&copy; 2020 | Attributions | Bradley James Sallee | " + days[d.getDay()] + ", " + + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
     
- }
-
-function experiment() {
-    document.getElementById("demo3").innerHTML = "Good stuff!";
-}
+ 
