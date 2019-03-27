be0cc41ee2c56efae04aad6799a9d13b3f50a91b
Fizzle99 <CharlieBJS@gmail.com>
2019-03-26 21:00:27 -0500

    commit

---
+++ assignments/lesson-11/weather-site/content/scripts/forecast.js
@@ -0,0 +1,16 @@
+/*Forecast JSON*/
+
+var section = document.querySelector('section');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+
+request.onload = function() {
+  var townValues = request.response;
+  showTowns(townValues);
+}
