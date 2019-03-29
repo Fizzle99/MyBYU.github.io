7747c3d1fa116e146f613892fa35cf32ab06fbf2
Fizzle99 <CharlieBJS@gmail.com>
2019-03-28 20:51:19 -0500

    commmit

--- assignments/lesson-11/weather-site/content/scripts/forecast.js
+++ assignments/lesson-11/weather-site/content/scripts/forecast.js
@@ -6,12 +6,7 @@ weatherRequest.open('GET', apiURLstring, true);
 weatherRequest.send();
 
 weatherRequest.onload = function() {
-var weatherRequestData = 
     for (var i = 0; i < weatherRequestData.list.length; i++) {
-        var x = forecastData.list[i].dt_txt;
-        var y = x.includes('18:00:00');
-        if (y == true) {
-           
-        } continue;
+        
      }
 }
